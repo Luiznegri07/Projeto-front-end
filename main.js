a8d46b1eb0749d13b39aadbbdf58a005202e8052
Matheus <124457479+Theuzxx@users.noreply.github.com>
2026-03-09 19:36:47 -0300

    ALPHA 3.0

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const icon = themeToggle.querySelector('.icon');
 const savedTheme = localStorage.getItem('theme');
 if (savedTheme === 'light') {
     body.classList.add('light-theme');
-    icon.textContent = '🌙'; // Muda o ícone para Lua
+    icon.textContent = '☀'; // Muda o ícone para Lua
 }
 
 themeToggle.addEventListener('click', () => {
@@ -16,7 +16,7 @@ themeToggle.addEventListener('click', () => {
     
     // Lógica para trocar ícone e salvar preferência
     if (body.classList.contains('light-theme')) {
-        icon.textContent = '🌙';
+        icon.textContent = '☀';
         localStorage.setItem('theme', 'light');
     } else {
         icon.textContent = '☀';
@@ -26,25 +26,40 @@ themeToggle.addEventListener('click', () => {
 
 //aviso de torneio ativo
 const tournamentDates = { 
-    "Counter-Strike 2": { name: "ESL PRO", month: 2 },  
-    "Valorant": { name: "Masters Santiago", month: 2 }
+    "Counter-Strike 2": { name: "ESL PRO LEAGUE", month: 2 },
+    "Valorant": { name: "Masters Santiago", month: 2 },
+    "LoL": { name: "CBLOL", month: 2 },
 };
 
 function checkEvents() {
+    // Pegamos o mês atual (0-11)
     const currentMonth = new Date().getMonth();
     const details = document.querySelectorAll('details');
 
     details.forEach(detail => {
-        const gameTitle = detail.querySelector('h2').textContent.replace(' ✪ ', '');
+        // .trim() remove espaços em branco acidentais nas pontas
+        const gameTitle = detail.querySelector('h2').textContent.replace(' ✪ ', '').trim();
         const event = tournamentDates[gameTitle];
 
+        // Verificamos se o evento existe e se o mês bate
         if (event && event.month === currentMonth) {
+            // Evita duplicar o badge se a função rodar duas vezes
+            if (detail.querySelector('.event-badge')) return;
+
             const badge = document.createElement('span');
             badge.className = 'event-badge';
             badge.innerText = ` ✦ Mês de ${event.name}!`;
             badge.style.color = "#d1b32b";
             badge.style.fontWeight = "bold";
+            detail.style.position = "relative";
+            badge.style.position = "absolute";
+            badge.style.top = "50px";
+            badge.style.bottom = "center";
+            
             detail.querySelector('summary').appendChild(badge);
         }
     });
 }
+
+// Garante que o código rode após o HTML estar pronto
+window.onload = checkEvents;
